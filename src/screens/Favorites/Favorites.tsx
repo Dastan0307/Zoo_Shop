936d6bebecc14bcf67d2091c418139bb2b356b6c
theflexor <nurdanokenov@gmail.com>
2023-05-06 23:59:58 +0600

    fix bug in fovorites

--- src/screens/Favorites/Favorites.tsx
+++ src/screens/Favorites/Favorites.tsx
@@ -3,28 +3,40 @@ import React, { useEffect, useState } from 'react'
 
 import { CardMain } from '@components/index'
 import { favorites } from '@store/favorites/favoriteId'
-import { AnnouncementCardType, CardType, FavoritesAnnounsmentType, FavoritesType } from '@typess/types'
+import {
+  AnnouncementCardType,
+  CardType,
+  FavoritesAnnounsmentType,
+  FavoritesType,
+} from '@typess/types'
 import { errorHandler } from '@utils/errorHandler'
 
 import api from '../../api/index'
+
 function Favorites() {
   const [announcement, setAnnouncement] = useState<FavoritesType[]>([])
- 
 
   useEffect(() => {
-    favorites().then(res => setAnnouncement(res))
+    favorites().then((res) => {
+      if (res) {
+        setAnnouncement(res)
+      }
+    })
   }, [])
 
-  console.log(announcement);
-  
+  console.log(announcement)
 
   return (
-    <div className='announcements'>
-      {
-        announcement && announcement[0] ? announcement.map(({photos, announsment} ) => 
-          <CardMain type='main' key={announsment.slug} value={{...announsment, photos}}/>
-        ) : null
-      }
+    <div className="announcements">
+      {announcement && announcement[0]
+        ? announcement.map(({ photos, announsment }) => (
+            <CardMain
+              type="main"
+              key={announsment.slug}
+              value={{ ...announsment, photos }}
+            />
+          ))
+        : null}
     </div>
   )
 }
